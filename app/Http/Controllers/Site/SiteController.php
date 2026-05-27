db8ebe0a5cefca4d9c2863de1e1052c53766d622
Isimbi Jade Gladys <gladysjade9@gmail.com>
2026-05-27 13:22:23 +0200

    CMS-30: add sites listing route and data flow

--- app/Http/Controllers/Site/SiteController.php
+++ app/Http/Controllers/Site/SiteController.php
@@ -6,22 +6,32 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Site\StoreSiteRequest;
 use App\Services\Site\SiteService;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 
 class SiteController extends Controller
 {
-    public function create(): View
-    {
-        return view('sites.create');
+    public function __construct(
+        protected SiteService $siteService
+    ) {
     }
 
-    public function store(StoreSiteRequest $request, SiteService $siteService)
+    public function index(Request $request): View
     {
-        $actor = $request->user();
+        $search = $request->string('search')->toString();
 
-        $siteService->createSite($request->validated(), $actor);
+        return view('sites.index', [
+            'sites' => $this->siteService->getPaginatedSites($search),
+            'search' => $search,
+        ]);
+    }
+
+    public function store(StoreSiteRequest $request): RedirectResponse
+    {
+        $this->siteService->createSite($request->validated(), $request->user());
 
         return redirect()
-            ->route('sites.create')
+            ->route('sites.index')
             ->with('success', 'Site created successfully.');
     }
 }
--- routes/site.php
+++ routes/site.php
@@ -4,8 +4,12 @@ use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'admin'])->group(function () {
-    Route::get('/admin/sites/create', [SiteController::class, 'create'])
-        ->name('sites.create');
+    Route::get('/admin/sites', [SiteController::class, 'index'])
+        ->name('sites.index');
+        
+    Route::get('/admin/sites/create', function () {
+        return redirect()->route('sites.index');
+    })->name('sites.create');
 
     Route::post('/admin/sites', [SiteController::class, 'store'])
         ->name('sites.store');

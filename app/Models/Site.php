17be4b4af0705460d3ffcff44e0b4cd841aca415
Isimbi Jade Gladys <gladysjade9@gmail.com>
2026-05-28 16:33:43 +0200

    CMS-71: remove site assignment logic

--- app/Models/Site.php
+++ app/Models/Site.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Site extends Model
@@ -33,18 +32,6 @@ class Site extends Model
     public function updatedBy(): BelongsTo
     {
         return $this->belongsTo(User::class, 'updated_by');
-    }
-
-    public function siteAssignments(): HasMany
-    {
-        return $this->hasMany(SiteAssignment::class);
-    }
-
-    public function assignedUsers(): BelongsToMany
-    {
-        return $this->belongsToMany(User::class, 'site_assignments')
-            ->withPivot(['id', 'assigned_by', 'updated_by'])
-            ->withTimestamps();
     }
 
     public function careers(): HasMany

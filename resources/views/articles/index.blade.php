5b6135c5e5cdcf15c1ecb23f081bb42137fba614
Isimbi Jade Gladys <gladysjade9@gmail.com>
2026-06-08 10:46:21 +0200

    CMS-82 refactor article creation flow to use the update page

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -78,9 +78,8 @@
                         Export
                     </button>
 
-                    <button
-                        type="button"
-                        id="openArticleCreateModal"
+                    <a
+                        href="{{ route('articles.create') }}"
                         class="flex items-center gap-2 rounded-2xl border border-blue-600 bg-blue-600 px-4 py-3 text-sm font-semibold text-white shadow-sm transition hover:-translate-y-0.5 hover:bg-blue-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500"
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" class="size-4 fill-current" viewBox="0 0 512 512" aria-hidden="true">
@@ -90,7 +89,7 @@
                             </g>
                         </svg>
                         Create Article Entry
-                    </button>
+                    </a>
                 </div>
             </div>
 
@@ -224,16 +223,6 @@
                                                     View
                                                 </a>
                                             @endif
-
-                                            <button
-                                                type="button"
-                                                class="inline-flex items-center gap-2 rounded-2xl border border-rose-200 bg-rose-50 px-3.5 py-2 text-xs font-semibold text-rose-700 shadow-sm transition hover:-translate-y-0.5 hover:border-rose-300 hover:bg-rose-100"
-                                            >
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.7" stroke="currentColor" class="size-5">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                                </svg>
-                                                Delete
-                                            </button>
                                         </div>
                                     </td>
                                 </tr>
@@ -300,8 +289,6 @@
             </div>
         </div>
     </div>
-
-    @include('articles.create-modal')
 @endsection
 
 @push('scripts')
--- routes/article.php
+++ routes/article.php
@@ -7,6 +7,9 @@ Route::middleware(['auth', 'cms.access'])->group(function () {
     Route::get('/admin/articles', [ArticleController::class, 'index'])
         ->name('articles.index');
 
+    Route::get('/admin/articles/create', [ArticleController::class, 'create'])
+        ->name('articles.create');
+
     Route::post('/admin/articles', [ArticleController::class, 'store'])
         ->name('articles.store');
 
